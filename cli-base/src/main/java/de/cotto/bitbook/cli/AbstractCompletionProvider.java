11c7325a8cf0f9093948bee8ad7e2b80acd9b66b
Carsten Otto <git@c-otto.de>
2021-04-10 19:28:06 +0100

    extract superclass with common logic

---
+++ cli-base/src/main/java/de/cotto/bitbook/cli/AbstractCompletionProvider.java
@@ -0,0 +1,98 @@
+package de.cotto.bitbook.cli;
+
+import com.google.common.base.Functions;
+import com.google.common.collect.Streams;
+import de.cotto.bitbook.backend.DescriptionService;
+import de.cotto.bitbook.backend.model.StringWithDescription;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.core.MethodParameter;
+import org.springframework.shell.CompletionContext;
+import org.springframework.shell.CompletionProposal;
+import org.springframework.shell.standard.ValueProviderSupport;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+public abstract class AbstractCompletionProvider<T extends StringWithDescription<T>> extends ValueProviderSupport {
+    private static final int MINIMUM_LENGTH_FOR_COMPLETION = 3;
+    protected final DescriptionService<T> descriptionService;
+
+    public AbstractCompletionProvider(DescriptionService<T> descriptionService) {
+        super();
+        this.descriptionService = descriptionService;
+    }
+
+    @Override
+    public List<CompletionProposal> complete(
+            MethodParameter methodParameter,
+            CompletionContext completionContext,
+            String[] hints
+    ) {
+        String input = completionContext.currentWordUpToCursor();
+        if (isTooShort(input, MINIMUM_LENGTH_FOR_COMPLETION)) {
+            return List.of();
+        }
+        return getCompletionProposals(input)
+                .collect(toMap(CompletionProposal::value, Functions.identity(), (a, b) -> a))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(toList());
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    protected boolean isTooShort(String input, int minimumLengthForCompletion) {
+        return input.length() < minimumLengthForCompletion;
+    }
+
+    protected abstract Set<Function<String, Set<String>>> getStringCompleters();
+
+    private Stream<CompletionProposal> getCompletionProposals(String input) {
+        Stream<CompletionProposal> completedInput = getStringCompleters().stream()
+                .map(completer -> completer.apply(input))
+                .flatMap(this::toProposals);
+        Stream<CompletionProposal> completedDescription = completeDescription(input);
+        return Streams.concat(completedInput, completedDescription);
+    }
+
+    protected Stream<CompletionProposal> toProposals(Set<String> strings) {
+        return strings.stream()
+                .map(descriptionService::get)
+                .filter(this::shouldConsider)
+                .map(this::getCompletionProposal);
+    }
+
+    protected abstract boolean shouldConsider(T stringWithDescription);
+
+    private CompletionProposal getCompletionProposal(T stringWithDescription) {
+        String description = stringWithDescription.getDescription();
+        CompletionProposal completionProposal = new CompletionProposal(stringWithDescription.getString());
+        if (description.isEmpty()) {
+            return completionProposal;
+        }
+        return completionProposal.description(description);
+    }
+
+    private Stream<CompletionProposal> completeDescription(String input) {
+        return descriptionService.getWithDescriptionInfix(input).stream()
+                .map(this::getCompletionProposalWithDescriptionInValue);
+    }
+
+    private CompletionProposal getCompletionProposalWithDescriptionInValue(T stringWithDescription) {
+        // JLine does not show completion proposals that don't contain the text typed in by the user.
+        // To work around this, we just include the description in the actual value (and remove it later).
+        String string = stringWithDescription.getString();
+        String separator = "\u00a0";
+        String description = "(" + stringWithDescription.getDescription() + ")";
+        String ansiDescription = AnsiOutput.toString(AnsiColor.BRIGHT_BLACK, description, AnsiColor.DEFAULT);
+        return new CompletionProposal(string + separator + ansiDescription);
+    }
+}
--- cli-base/src/main/java/de/cotto/bitbook/cli/AbstractTransactionCompletionProvider.java
+++ cli-base/src/main/java/de/cotto/bitbook/cli/AbstractTransactionCompletionProvider.java
@@ -1,95 +1,30 @@
 package de.cotto.bitbook.cli;
 
-import com.google.common.base.Functions;
-import com.google.common.collect.Streams;
 import de.cotto.bitbook.backend.TransactionDescriptionService;
 import de.cotto.bitbook.backend.model.TransactionWithDescription;
 import de.cotto.bitbook.backend.transaction.TransactionCompletionDao;
-import org.springframework.boot.ansi.AnsiColor;
-import org.springframework.boot.ansi.AnsiOutput;
-import org.springframework.core.MethodParameter;
-import org.springframework.shell.CompletionContext;
-import org.springframework.shell.CompletionProposal;
-import org.springframework.shell.standard.ValueProviderSupport;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-
-public abstract class AbstractTransactionCompletionProvider extends ValueProviderSupport {
-    private static final int MINIMUM_LENGTH_FOR_COMPLETION = 3;
+public abstract class AbstractTransactionCompletionProvider
+        extends AbstractCompletionProvider<TransactionWithDescription> {
 
     private final TransactionCompletionDao transactionCompletionDao;
-    private final TransactionDescriptionService transactionDescriptionService;
 
     public AbstractTransactionCompletionProvider(
             TransactionCompletionDao transactionCompletionDao,
             TransactionDescriptionService transactionDescriptionService
     ) {
-        super();
+        super(transactionDescriptionService);
         this.transactionCompletionDao = transactionCompletionDao;
-        this.transactionDescriptionService = transactionDescriptionService;
     }
 
     @Override
-    public List<CompletionProposal> complete(
-            MethodParameter methodParameter,
-            CompletionContext completionContext,
-            String[] hints
-    ) {
-        String input = completionContext.currentWordUpToCursor();
-        if (input.length() < MINIMUM_LENGTH_FOR_COMPLETION) {
-            return List.of();
-        }
-        Stream<CompletionProposal> fromTransactionDetails = toProposals(
-                transactionCompletionDao.completeFromTransactionDetails(input)
+    protected Set<Function<String, Set<String>>> getStringCompleters() {
+        return Set.of(
+                transactionCompletionDao::completeFromTransactionDetails,
+                transactionCompletionDao::completeFromAddressTransactionHashes
         );
-        Stream<CompletionProposal> fromAddressTransactionHashes = toProposals(
-                transactionCompletionDao.completeFromAddressTransactionHashes(input)
-        );
-        Stream<CompletionProposal> completedDescriptions =
-                transactionDescriptionService.getWithDescriptionInfix(input).stream()
-                        .map(this::getCompletionProposalWithDescriptionInValue);
-        return Streams.concat(fromTransactionDetails, fromAddressTransactionHashes, completedDescriptions)
-                .collect(toMap(CompletionProposal::value, Functions.identity(), (a, b) -> a))
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .map(Map.Entry::getValue)
-                .collect(toList());
     }
-
-    private Stream<CompletionProposal> toProposals(Set<String> transactionHashes) {
-        return transactionHashes.stream()
-                .map(transactionDescriptionService::get)
-                .filter(this::shouldConsider)
-                .map(this::getCompletionProposal);
-    }
-
-    private CompletionProposal getCompletionProposal(TransactionWithDescription transactionWithDescription) {
-        String description = transactionWithDescription.getDescription();
-        CompletionProposal completionProposal = new CompletionProposal(transactionWithDescription.getTransactionHash());
-        if (description.isEmpty()) {
-            return completionProposal;
-        }
-        return completionProposal.description(description);
-    }
-
-    private CompletionProposal getCompletionProposalWithDescriptionInValue(
-            TransactionWithDescription transactionWithDescription
-    ) {
-        // JLine does not show completion proposals that don't contain the text typed in by the user.
-        // To work around this, we just include the description in the actual value (and remove it later).
-        String address = transactionWithDescription.getTransactionHash();
-        String separator = "\u00a0";
-        String description = "(" + transactionWithDescription.getDescription() + ")";
-        String ansiDescription = AnsiOutput.toString(AnsiColor.BRIGHT_BLACK, description, AnsiColor.DEFAULT);
-        return new CompletionProposal(address + separator + ansiDescription);
-    }
-
-    protected abstract boolean shouldConsider(TransactionWithDescription transactionWithDescription);
 }
