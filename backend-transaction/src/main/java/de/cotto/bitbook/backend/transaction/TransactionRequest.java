5b03b2df00069e679eac81ef098cf4288f5d2f30
Carsten Otto <git@c-otto.de>
2021-04-23 20:29:44 +0100

    remove dead code

--- backend-transaction/src/main/java/de/cotto/bitbook/backend/transaction/TransactionRequest.java
+++ backend-transaction/src/main/java/de/cotto/bitbook/backend/transaction/TransactionRequest.java
@@ -4,15 +4,9 @@ import de.cotto.bitbook.backend.request.PrioritizedRequest;
 import de.cotto.bitbook.backend.request.RequestPriority;
 import de.cotto.bitbook.backend.transaction.model.Transaction;
 
-import java.util.function.Consumer;
-
 public final class TransactionRequest extends PrioritizedRequest<String, Transaction> {
     public TransactionRequest(String transactionHash, RequestPriority priority) {
-        this(transactionHash, priority, result -> {});
-    }
-
-    public TransactionRequest(String transactionHash, RequestPriority priority, Consumer<Transaction> resultConsumer) {
-        super(transactionHash, priority, resultConsumer);
+        super(transactionHash, priority, result -> {});
     }
 
     public String getHash() {
