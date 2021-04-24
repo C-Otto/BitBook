e8517fb7cc1372ef9fae2338ad32fdc7d8b1c160
Carsten Otto <git@c-otto.de>
2021-04-24 21:22:49 +0100

    rename application class

--- cli/src/integrationTest/java/de/cotto/bitbook/BitBookApplicationIT.java
+++ cli/src/integrationTest/java/de/cotto/bitbook/BitBookApplicationIT.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class TransactionsApplicationIT {
+class BitBookApplicationIT {
     @Autowired
     private OwnershipCommands ownershipCommands;
 
