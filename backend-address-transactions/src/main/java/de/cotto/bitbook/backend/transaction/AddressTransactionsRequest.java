12b39cd96d6a8e257f316fcf7074f072b4b760d4
Carsten Otto <git@c-otto.de>
2021-04-26 13:57:20 +0100

    remove unused methods

--- backend-address-transactions/src/main/java/de/cotto/bitbook/backend/transaction/AddressTransactionsRequest.java
+++ backend-address-transactions/src/main/java/de/cotto/bitbook/backend/transaction/AddressTransactionsRequest.java
@@ -4,20 +4,9 @@ import de.cotto.bitbook.backend.request.PrioritizedRequest;
 import de.cotto.bitbook.backend.request.RequestPriority;
 import de.cotto.bitbook.backend.transaction.model.AddressTransactions;
 
-import static de.cotto.bitbook.backend.request.RequestPriority.LOWEST;
-import static de.cotto.bitbook.backend.request.RequestPriority.STANDARD;
-
 public final class AddressTransactionsRequest extends PrioritizedRequest<TransactionsRequestKey, AddressTransactions> {
     private AddressTransactionsRequest(TransactionsRequestKey transactionsRequestKey, RequestPriority priority) {
         super(transactionsRequestKey, priority);
-    }
-
-    public static AddressTransactionsRequest forLowestPriority(TransactionsRequestKey transactionsRequestKey) {
-        return create(transactionsRequestKey, LOWEST);
-    }
-
-    public static AddressTransactionsRequest forStandardPriority(TransactionsRequestKey transactionsRequestKey) {
-        return create(transactionsRequestKey, STANDARD);
     }
 
     public static AddressTransactionsRequest create(
--- backend-address-transactions/src/testFixtures/java/de/cotto/bitbook/backend/transaction/TransactionsRequestKeyFixtures.java
+++ backend-address-transactions/src/testFixtures/java/de/cotto/bitbook/backend/transaction/TransactionsRequestKeyFixtures.java
@@ -1,5 +1,7 @@
 package de.cotto.bitbook.backend.transaction;
 
+import static de.cotto.bitbook.backend.request.RequestPriority.LOWEST;
+import static de.cotto.bitbook.backend.request.RequestPriority.STANDARD;
 import static de.cotto.bitbook.backend.transaction.model.AddressTransactionsFixtures.ADDRESS;
 import static de.cotto.bitbook.backend.transaction.model.TransactionFixtures.BLOCK_HEIGHT;
 
@@ -7,7 +9,7 @@ public class TransactionsRequestKeyFixtures {
     public static final TransactionsRequestKey TRANSACTIONS_REQUEST_KEY =
             new TransactionsRequestKey(ADDRESS, BLOCK_HEIGHT);
     public static final AddressTransactionsRequest ADDRESS_TRANSACTIONS_REQUEST =
-            AddressTransactionsRequest.forStandardPriority(TRANSACTIONS_REQUEST_KEY);
+            AddressTransactionsRequest.create(TRANSACTIONS_REQUEST_KEY, STANDARD);
     public static final AddressTransactionsRequest ADDRESS_TRANSACTIONS_LOWEST =
-            AddressTransactionsRequest.forLowestPriority(TRANSACTIONS_REQUEST_KEY);
+            AddressTransactionsRequest.create(TRANSACTIONS_REQUEST_KEY, LOWEST);
 }
