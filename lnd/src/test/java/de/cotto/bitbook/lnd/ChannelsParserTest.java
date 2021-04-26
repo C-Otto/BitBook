524dfa66106128556faac65c5e865fd4fbf5b2e7
Carsten Otto <git@c-otto.de>
2021-04-26 23:05:37 +0100

    load transaction details in bulk, add test

--- lnd/src/test/java/de/cotto/bitbook/lnd/ChannelsParserTest.java
+++ lnd/src/test/java/de/cotto/bitbook/lnd/ChannelsParserTest.java
@@ -7,16 +7,20 @@ import de.cotto.bitbook.backend.transaction.TransactionService;
 import de.cotto.bitbook.lnd.model.Channel;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.Set;
 
 import static de.cotto.bitbook.backend.transaction.model.TransactionFixtures.TRANSACTION;
 import static de.cotto.bitbook.lnd.model.ChannelFixtures.OPENING_TRANSACTION;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,6 +49,7 @@ public class ChannelsParserTest {
 
     @Test
     void parses_channel_and_gets_transaction_details() throws IOException {
+        when(transactionService.getTransactionDetails(Set.of("somewhere"))).thenReturn(Set.of());
         when(transactionService.getTransactionDetails("somewhere")).thenReturn(OPENING_TRANSACTION);
         String json = "{\"channels\":[" +
                       "{" +
@@ -59,12 +64,25 @@ public class ChannelsParserTest {
 
     @Test
     void parses_many_channels() throws IOException {
+        when(transactionService.getTransactionDetails(Set.of("a", "b"))).thenReturn(Set.of());
         when(transactionService.getTransactionDetails(anyString())).thenReturn(TRANSACTION);
         String json = "{\"channels\":[" +
                       "{\"remote_pubkey\":\"pubkey\", \"initiator\": true, \"channel_point\": \"a:1\"}," +
                       "{\"remote_pubkey\":\"pubkey2\", \"initiator\": false, \"channel_point\": \"b:2\"}" +
                       "]}";
         assertThat(channelsParser.parse(toJsonNode(json))).hasSize(2);
+    }
+
+    @Test
+    void preloads_transaction_details() throws IOException {
+        String json = "{\"channels\":[" +
+                      "{\"remote_pubkey\":\"pubkey\", \"initiator\": true, \"channel_point\": \"a:1\"}," +
+                      "{\"remote_pubkey\":\"pubkey2\", \"initiator\": false, \"channel_point\": \"b:2\"}" +
+                      "]}";
+        channelsParser.parse(toJsonNode(json));
+        InOrder inOrder = Mockito.inOrder(transactionService);
+        inOrder.verify(transactionService).getTransactionDetails(Set.of("a", "b"));
+        inOrder.verify(transactionService, times(2)).getTransactionDetails(anyString());
     }
 
     private JsonNode toJsonNode(String json) throws IOException {
