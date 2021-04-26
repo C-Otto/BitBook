1b9db76548d959348567621ee128e8cec32cdfee
Carsten Otto <git@c-otto.de>
2021-04-26 18:20:28 +0100

    add tests

--- backend-transaction/src/test/java/de/cotto/bitbook/backend/transaction/persistence/OutputJpaDtoTest.java
+++ backend-transaction/src/test/java/de/cotto/bitbook/backend/transaction/persistence/OutputJpaDtoTest.java
@@ -3,6 +3,7 @@ package de.cotto.bitbook.backend.transaction.persistence;
 import org.junit.jupiter.api.Test;
 
 import static de.cotto.bitbook.backend.transaction.model.OutputFixtures.OUTPUT_1;
+import static de.cotto.bitbook.backend.transaction.model.OutputFixtures.OUTPUT_ADDRESS_1;
 import static de.cotto.bitbook.backend.transaction.persistence.OutputJpaDtoFixtures.OUTPUT_JPA_DTO_1;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +16,10 @@ class OutputJpaDtoTest {
     @Test
     void fromModel() {
         assertThat(OutputJpaDto.fromModel(OUTPUT_1)).usingRecursiveComparison().isEqualTo(OUTPUT_JPA_DTO_1);
+    }
+
+    @Test
+    void getAddress() {
+        assertThat(OUTPUT_JPA_DTO_1.getAddress()).isEqualTo(OUTPUT_ADDRESS_1);
     }
 }
