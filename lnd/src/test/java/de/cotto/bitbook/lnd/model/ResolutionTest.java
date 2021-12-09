46852932aa5567c481360983d19dcc1d01a302b8
Carsten Otto <git@c-otto.de>
2021-12-09 22:23:51 +0100

    add sweep test cases

--- lnd/src/test/java/de/cotto/bitbook/lnd/model/ResolutionTest.java
+++ lnd/src/test/java/de/cotto/bitbook/lnd/model/ResolutionTest.java
@@ -8,16 +8,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ResolutionTest {
 
     private static final String COMMIT = "COMMIT";
+    private static final String ANCHOR = "ANCHOR";
     private static final String CLAIMED = "CLAIMED";
+    private static final String FIRST_STAGE = "FIRST_STAGE";
     private static final String TIMEOUT = "TIMEOUT";
     private static final String INCOMING_HTLC = "INCOMING_HTLC";
     private static final String OUTGOING_HTLC = "OUTGOING_HTLC";
     private static final String SWEEP_TRANSACTION_HASH = "bar";
     private static final Resolution RESOLUTION_COMMIT_CLAIMED = new Resolution(SWEEP_TRANSACTION_HASH, COMMIT, CLAIMED);
+    private static final Resolution RESOLUTION_ANCHOR_CLAIMED = new Resolution(SWEEP_TRANSACTION_HASH, ANCHOR, CLAIMED);
 
     @Test
     void sweepTransactionClaimsFunds_commit_claimed() {
         assertThat(RESOLUTION_COMMIT_CLAIMED.sweepTransactionClaimsFunds()).isTrue();
+    }
+
+    @Test
+    void sweepTransactionClaimsFunds_anchor_claimed() {
+        assertThat(RESOLUTION_ANCHOR_CLAIMED.sweepTransactionClaimsFunds()).isTrue();
     }
 
     @Test
@@ -41,6 +49,12 @@ class ResolutionTest {
     @Test
     void sweepTransactionClaimsFunds_outgoing_htlc_timeout() {
         assertThat(new Resolution(SWEEP_TRANSACTION_HASH, OUTGOING_HTLC, TIMEOUT)
+                .sweepTransactionClaimsFunds()).isTrue();
+    }
+
+    @Test
+    void sweepTransactionClaimsFunds_outgoing_htlc_first_stage() {
+        assertThat(new Resolution(SWEEP_TRANSACTION_HASH, OUTGOING_HTLC, FIRST_STAGE)
                 .sweepTransactionClaimsFunds()).isTrue();
     }
 
