7f30783eb8343e04c749b9240567aede73b229fe
Carsten Otto <git@c-otto.de>
2022-01-24 16:38:41 +0100

    get block height from bitcoind via bitcoin-cli
    related to #4

---
+++ backend/provider/bitcoind/src/test/java/de/cotto/bitbook/backend/transaction/bitcoind/BitcoinCliWrapperTest.java
@@ -0,0 +1,46 @@
+package de.cotto.bitbook.backend.transaction.bitcoind;
+
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+import static de.cotto.bitbook.backend.transaction.model.TransactionFixtures.BLOCK_HEIGHT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BitcoinCliWrapperTest {
+    private final TestableBitcoinCliWrapper bitcoinCliWrapper = new TestableBitcoinCliWrapper();
+
+    @Test
+    void getBlockCount_error() {
+        assertThat(bitcoinCliWrapper.getBlockCount()).isEmpty();
+    }
+
+    @Test
+    void getBlockCount_empty_string() {
+        bitcoinCliWrapper.result = "";
+        assertThat(bitcoinCliWrapper.getBlockCount()).isEmpty();
+    }
+
+    @Test
+    void getBlockCount() {
+        bitcoinCliWrapper.result = BLOCK_HEIGHT + "\n";
+        assertThat(bitcoinCliWrapper.getBlockCount()).contains(BLOCK_HEIGHT);
+        assertThat(bitcoinCliWrapper.command).contains("bitcoin-cli", "getblockcount");
+    }
+
+    @SuppressWarnings("PMD.ArrayIsStoredDirectly")
+    private static class TestableBitcoinCliWrapper extends BitcoinCliWrapper {
+        @Nullable
+        private String result;
+
+        @Nullable
+        private String[] command;
+
+        @Override
+        protected Optional<String> execute(String... command) {
+            this.command = command;
+            return Optional.ofNullable(result);
+        }
+    }
+}
