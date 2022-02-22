cb19620cf29f29923cbf239e1a66e3a2a2f6bab7
Carsten Otto <git@c-otto.de>
2022-02-22 11:37:34 +0100

    add test: fail requests for non-BTC chains

--- backend/provider/electrs/src/test/java/de/cotto/bitbook/backend/transaction/electrs/ElectrsAddressTransactionsProviderTest.java
+++ backend/provider/electrs/src/test/java/de/cotto/bitbook/backend/transaction/electrs/ElectrsAddressTransactionsProviderTest.java
@@ -1,6 +1,7 @@
 package de.cotto.bitbook.backend.transaction.electrs;
 
 import de.cotto.bitbook.backend.model.Address;
+import de.cotto.bitbook.backend.model.ProviderException;
 import de.cotto.bitbook.backend.transaction.TransactionsRequestKey;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,12 +17,14 @@ import static de.cotto.bitbook.backend.model.AddressTransactionsFixtures.ADDRESS
 import static de.cotto.bitbook.backend.model.AddressTransactionsFixtures.ADDRESS_TRANSACTIONS_UPDATED;
 import static de.cotto.bitbook.backend.model.AddressTransactionsFixtures.LAST_CHECKED_AT_BLOCK_HEIGHT;
 import static de.cotto.bitbook.backend.model.Chain.BCH;
+import static de.cotto.bitbook.backend.model.Chain.BSV;
 import static de.cotto.bitbook.backend.model.Chain.BTC;
 import static de.cotto.bitbook.backend.model.TransactionHashFixtures.TRANSACTION_HASH;
 import static de.cotto.bitbook.backend.model.TransactionHashFixtures.TRANSACTION_HASH_2;
 import static de.cotto.bitbook.backend.model.TransactionHashFixtures.TRANSACTION_HASH_3;
 import static de.cotto.bitbook.backend.model.TransactionHashFixtures.TRANSACTION_HASH_4;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +46,13 @@ class ElectrsAddressTransactionsProviderTest {
     void isSupported_bch() {
         TransactionsRequestKey key = new TransactionsRequestKey(new Address("abc)"), BCH, 123);
         assertThat(provider.isSupported(key)).isFalse();
+    }
+
+    @Test
+    void get_unsupported_chain() {
+        assertThatExceptionOfType(ProviderException.class).isThrownBy(
+                () -> provider.get(new TransactionsRequestKey(ADDRESS, BSV, LAST_CHECKED_AT_BLOCK_HEIGHT))
+        );
     }
 
     @Test
