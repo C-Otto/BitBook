fa23632a398a656ec94ef5e9f55dc520f058466c
Carsten Otto <git@c-otto.de>
2022-11-24 13:29:22 +0100

    update spring boot

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.0.1")
     implementation("net.ltgt.gradle:gradle-nullaway-plugin:1.5.0")
     implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.0.13")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.13")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.14")
     implementation("com.adarshr:gradle-test-logger-plugin:3.2.0")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.9.0")
 }
